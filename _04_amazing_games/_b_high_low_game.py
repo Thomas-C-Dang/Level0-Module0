00f1e9e9a3a32cf9aa4ae4eb631e23bebfe8ffd0
Thomas-C-Dang <tcdang123@gmail.com>
2022-06-22 10:24:44 -0700

    Currently working on: N/A (COMPLETE!)

--- _04_amazing_games/_b_high_low_game.py
+++ _04_amazing_games/_b_high_low_game.py
@@ -17,24 +17,30 @@ if __name__ == '__main__':
     # 3. Code a for loop to run steps 4-10, 10 times
     for i in range(10):
         print(random_num)
+        if answer == random_num:
+            exit()
+        if answer > random_num:
+            answer = simpledialog.askinteger(title='Too high!', prompt="Too high")
+        if answer < random_num:
+            answer = simpledialog.askinteger(title='Too low!', prompt="Too low")
+
+    messagebox.showinfo(title='Defeat', message="Lmao you lost")
+    window.mainloop()
         # 4. Ask the user for a guess using a pop-up window, and save their response
-    window = Tk()
-    window.withdraw()
+
 
         # 5. If the guess is correct
-    if answer == random_num:
-        sys.exit(0)
+
             # 6. Win. Use 'sys.exit(0)' to end the program
 
         # 7. if the guess is high
-    if answer > random_num:
-       messagebox.showinfo(title='Too high!', message="Too high")
-    else:
-        messagebox.showinfo(itle='Too low!', message="Too low")
+    #if answer > random_num:
+       #messagebox.showinfo(title='Too high!', message="Too high")
+    #else:
+        #messagebox.showinfo(itle='Too low!', message="Too low")
             # 8. Tell them it's too high
         # 9. Else if the guess is low
             # 10. Tell them it's too low
 
     #11. Outside of the loop, tell the user they lost
-messagebox.showinfo(title='Defeat', message="Lmao you lost")
-window.mainloop()
+
